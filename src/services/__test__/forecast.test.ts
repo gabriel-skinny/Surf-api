c1af34dcaf7698761ccef339078b0f2a561ceb19
Gabriel Vasconcelos Catoni <gabriel.vasconcelos@cbdinvestimentos.com>
2021-12-30 17:23:43 -0300

    Finalizando teste de integraçao

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,7 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
+import { Beach, BeachPosition } from '@src/models/beach';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_weather_3_hours.json';
-import exp from 'constants';
-import { Beach, BeachPosition, Forecast, ForecastProcessingInternalError } from '../forecast';
+import { Forecast, ForecastProcessingInternalError } from '../forecast';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -17,7 +17,6 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
-        user: 'some-id',
       },
     ];
     const expectedResponse = [
@@ -101,7 +100,6 @@ describe('Forecast Service', () => {
       lng: 151.289824,
       name: 'Manly',
       position: BeachPosition.E,
-      user: 'some-id',
     }
   ]
 
