81c4bbd4ed4680e390a799d6985b16da72b73c77
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-01-01 20:01:29 -0300

    Formatando request util

--- src/util/request.ts
+++ src/util/request.ts
@@ -1,20 +1,17 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
-export interface RequestConfig extends AxiosRequestConfig {};
+export interface RequestConfig extends AxiosRequestConfig {}
 
-export interface Response<T=any> extends AxiosResponse<T> {};
+export interface Response<T = any> extends AxiosResponse<T> {}
 
 export class Request {
-  constructor(private request = axios) {
+  constructor(private request = axios) {}
 
-  }
-
-  public get<T>(url: string, config: RequestConfig = {}):Promise<Response<T>> {
+  public get<T>(url: string, config: RequestConfig = {}): Promise<Response<T>> {
     return this.request.get<T, Response<T>>(url, config);
-
   }
 
-  public static isRequestError(error: AxiosError):boolean {
+  public static isRequestError(error: AxiosError): boolean {
     return !!(error.response && error.response.status);
   }
 }
