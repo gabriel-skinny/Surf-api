1efdf61ba666656beef0f65a11fd8ab1e234c99c
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-01-01 22:01:13 -0300

    Adicionando id-fake no mock de beaches

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -18,6 +18,7 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
+        user: "fake-id"
       },
     ];
     const expectedResponse = [
@@ -101,6 +102,7 @@ describe('Forecast Service', () => {
         lng: 151.289824,
         name: 'Manly',
         position: BeachPosition.E,
+        user: "fake-id"
       },
     ];
 
