bd0b9954a944dfe1a4919e742b2e5a9638b9b88a
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-01-01 22:00:24 -0300

    Adicionando metodos de generar token e decodar token

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,25 +1,34 @@
-import { hash, compare } from 'bcrypt';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { get } from "config";
+import config from "config";
+import { User } from '@src/models/user';
+
+export interface DecodedUser extends Omit<User, "_id"> {
+  id: string;
+}
 
 export class Auth {
   public static async hashPassword(
     password: string,
     salt = 10
   ): Promise<string> {
-    return await hash(password, salt);
+    return await bcrypt.hash(password, salt);
   }
 
   public static async comparePassword(
     password: string,
     hashedpassword: string
   ): Promise<boolean> {
-    return await compare(password, hashedpassword);
+    return await bcrypt.compare(password, hashedpassword);
   }
 
   public static generateToken(payload: object): string {
-    return jwt.sign(payload, get("App.auth.key"), {
-      expiresIn: get("App.auth.tokenExpiresIn")
+    return jwt.sign(payload, config.get("App.auth.key"), {
+      expiresIn: config.get("App.auth.tokenExpiresIn")
     })
+  }
+
+  public static decodeToken(token: string): DecodedUser {
+    return jwt.verify(token, config.get("App.auth.key")) as DecodedUser;
   }
 }
