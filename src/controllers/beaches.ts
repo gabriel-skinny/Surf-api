95e0c43d282411b843eced747e5c2402d7746d29
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-01-01 21:58:45 -0300

    Autenticando rota e relacionando praia com usuarios

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,15 +1,17 @@
-import { Controller, Post } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Post } from '@overnightjs/core';
+import { authMiddleware } from '@src/middlewares/auth';
 import { BeachDb } from '@src/models/beach';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { BaseController } from '.';
 
 @Controller('beaches')
+@ClassMiddleware(authMiddleware)
 export class BeachController extends BaseController {
   @Post('')
   public async createBeach(req: Request, res: Response): Promise<void> {
     try {
-      const beach = new BeachDb(req.body);
+      const beach = new BeachDb({...req.body, user: req.decoded?.id});
       const result = await beach.save();
 
       res.status(201).send(result);
